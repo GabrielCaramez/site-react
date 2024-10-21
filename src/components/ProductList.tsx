8bc4e42f10dc582b8306cd53e169d1f6be5143c7
GABRIEL CARAMEZ BENVINDO DA SILVA <155910430+GabrielCaramez@users.noreply.github.com>
2024-10-21 15:54:11 -0300

    add imagen

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import nintendo from '../assets/nintendo.png';
 
 interface ProductListProps {
   addToCart: (id: number) => void;
 }
 
 const products = [
-  { id: 1, name: 'Nintendo Switch', price: 1000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 2, name: 'Nintendo Switch', price: 2000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 3, name: 'Nintendo Switch', price: 3000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 4, name: 'Nintendo Switch', price: 4000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 5, name: 'Nintendo Switch', price: 5000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 6, name: 'Nintendo Switch', price: 6000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 7, name: 'Nintendo Switch', price: 7000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 8, name: 'Nintendo Switch', price: 8000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 9, name: 'Nintendo Switch', price: 9000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 10, name: 'Nintendo Switch', price: 10000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 11, name: 'Nintendo Switch', price: 11000.0, imageUrl: 'https://via.placeholder.com/150' },
-  { id: 12, name: 'Nintendo Switch', price: 12000.0, imageUrl: 'https://via.placeholder.com/150' },
+  { id: 2, name: 'Nintendo Switch', price: 2000.0, imageUrl: nintendo },
+  { id: 3, name: 'Nintendo Switch', price: 3000.0, imageUrl: nintendo },
+  { id: 4, name: 'Nintendo Switch', price: 4000.0, imageUrl: nintendo },
+  { id: 5, name: 'Nintendo Switch', price: 5000.0, imageUrl: nintendo },
+  { id: 1, name: 'Nintendo Switch', price: 1000.0, imageUrl: nintendo },
+  { id: 6, name: 'Nintendo Switch', price: 6000.0, imageUrl: nintendo },
+  { id: 7, name: 'Nintendo Switch', price: 7000.0, imageUrl: nintendo },
+  { id: 8, name: 'Nintendo Switch', price: 8000.0, imageUrl: nintendo },
+  { id: 9, name: 'Nintendo Switch', price: 9000.0, imageUrl: nintendo },
+  { id: 10, name: 'Nintendo Switch', price: 10000.0, imageUrl: nintendo },
+  { id: 11, name: 'Nintendo Switch', price: 11000.0, imageUrl: nintendo },
+  { id: 12, name: 'Nintendo Switch', price: 12000.0, imageUrl: nintendo },
 ];
 
 const ProductList: React.FC<ProductListProps> = ({ addToCart }) => {
@@ -29,7 +30,7 @@ const ProductList: React.FC<ProductListProps> = ({ addToCart }) => {
             alt={product.name}
             className="product-image"
             onError={(e) => {
-              e.currentTarget.src = 'https://via.placeholder.com/150';
+              e.currentTarget.src = nintendo;
             }}
           />
           <div className="product-details">
